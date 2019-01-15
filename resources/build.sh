7d9011447f9f6356c472af3ccd9df446574ffea7
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2019-01-15 13:43:01 +0530

    Add error handling function in build.sh
    * Updated `build.sh` with SystemC-2.3.3 Release.
    * Added function to handle error at any steps.
    * Updated resource links in `links.txt`.

--- resources/build.sh
+++ resources/build.sh
@@ -1,53 +1,82 @@
 #!/bin/bash
 
-FILE_PREFIX="systemc"
-FILE_VERSION="2.3.2"
+SRC_DIR=systemc-2.3.3
+ZIP_FILE=$SRC_DIR.tar.gz
+DOWNLOAD_URL=https://www.accellera.org/images/downloads/standards/systemc/$ZIP_FILE
 
-DIRECTORY_NAME="$FILE_PREFIX-$FILE_VERSION"
+BUILD_TYPE=Release
+
+function stepTry {
+  "$@"
+  local step_status=$?
+  if [ $step_status -ne 0 ]; then
+    echo "Error: $1" >&2
+  fi
+  return $step_status
+}
 
 # This script will download the SystemC library from Accelera website and configure the build and build it for you.
-echo "======================================================"
-echo "Downloading the SystemC library from accellera website"
-echo "======================================================"
-wget -c http://accellera.org/images/downloads/standards/systemc/$DIRECTORY_NAME.tar.gz
+if [ -f "$ZIP_FILE" ]; then
+    echo "======================================================"
+    echo "Downloaded file exists"
+    echo "======================================================"
+else
+    echo "======================================================"
+    echo "Downloading the SystemC library from accellera website"
+    echo "======================================================"
+    stepTry wget -c "$DOWNLOAD_URL"
+fi
 
-echo "======================================================"
-echo "Extracting the SystemC source code"
-echo "======================================================"
-tar xf $DIRECTORY_NAME.tar.gz
+if [ -d "$SRC_DIR" ]; then
+    echo "======================================================"
+    echo " Source Directory Exists."
+    echo "======================================================"
+else
+    echo "======================================================"
+    echo "Extracting the SystemC source code"
+    echo "======================================================"
+    stepTry tar xf $ZIP_FILE
+fi
 
 pd=$PWD
 
-cd $DIRECTORY_NAME
+steptry cd $SRC_DIR
 
 echo "======================================================"
 echo "Creating build directory"
 echo "======================================================"
-if [ -d "objdir" ]; then
+if [ -d "build" ]; then
 	echo "!!!Directory Already Exists!!!"
 else
-	mkdir objdir
+	stepTry mkdir build
 fi
 
-cd objdir
+stepTry cd build
 
 # Create Installation Directory
-SYSTEMC_HOME=$HOME/apps/$DIRECTORY_NAME
-mkdir -p $HOME/apps/$DIRECTORY_NAME
+SYSTEMC=$HOME/apps/$SRC_DIR
+stepTry mkdir -p $SYSTEMC
 
 # Configure the SystemC library
 echo "============================================================================"
 echo "Configuring the SystemC library"
-echo "SystemC library will be installed in $SYSTEMC_HOME"
+echo "SystemC library will be installed in $SYSTEMC: $BUILD_TYPE"
 echo "============================================================================"
-#../configure --prefix=$SYSTEMC_HOME --enable-debug
-# Using CMake for much better integration and portability accross system with
-# different OS's.
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo    \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON   \
-      -DCMAKE_INSTALL_PREFIX=$SYSTEMC_HOME \
-      -DCMAKE_CXX_STANDARD=11              \
-      ..
+#if [ "$BUILD_TYPE" == "Debug" ]; then
+#    CONFIG_PARMETER="--enable-debug --disable-optimize"
+#elif [ "$BUILD_TYPE" == "RelWithDebInfo" ]; then
+#    CONFIG_PARAMETER="--enable-debug"
+#else
+#    CONFIG_PARAMTER=""
+#fi
+stepTry cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE     \
+              -DCMAKE_INSTALL_PREFIX=$SYSTEMC    \
+              -DCMAKE_CXX_STANDARD=11            \
+              -DCMAKE_CXX_EXTENSIONS=OFF         \
+              -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+              -DBUILD_SOURCE_DOCUMENTATION=ON    \
+              ..
+#../configure --prefix=$SYSTEMC $CONFIG_PARAMETER
 
 # Find the Number of CPUS to utilize all the cores to build the SystemC llibrary
 CPUS=`nproc`
@@ -59,33 +88,43 @@ echo "======================================================"
 echo "======================================================"
 echo "Building the SystemC library"
 echo "======================================================"
-make -j $CPUS
+stepTry make clean
+stepTry make -j $CPUS
 
 # Check the build SystemC library
 echo "==========================================================="
 echo "Checking the Built library by building examples application"
 echo "==========================================================="
-make check -j $CPUS
+stepTry make check
 
 # Do the Installation
 echo "======================================================"
 echo "SystemC Installation"
 echo "======================================================"
-make install
+stepTry make install
 
 cd $pd
 
-echo "====================================================================================================================================================="
-echo "Please Add the contents of this SystemC.config to the end of your .bashrc file located in your $HOME folder"
-echo "====================================================================================================================================================="
+#echo "====================================================================================================================================================="
+#echo "Please Add the contents of this SystemC.config to the end of your .bashrc file located in your $HOME folder"
+#echo "====================================================================================================================================================="
 
-echo "export SYSTEMC_HOME=$SYSTEMC_HOME" > SystemC.config
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} = "x86_64" ]; then
-	echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}\$SYSTEMC_HOME/lib-linux64:\$SYSTEMC_HOME/lib" >> SystemC.config
-else
-	echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}\$SYSTEMC_HOME/lib-linux:\$SYSTEMC_HOME/lib" >> SystemC.config
-fi
+#echo "export SYSTEMC=$SYSTEMC" > SystemC.config
+#echo "export SYSTEMC_HOME=\$SYSTEMC" >> SystemC.config
+#MACHINE_TYPE=`uname -m`
+#if [ ${MACHINE_TYPE} = "x86_64" ]; then
+#	echo "export TARGET_ARCH=linux64" >> SystemC.config
+#else
+#	echo "export TARGET_ARCH=linux" >> SystemC.config
+#fi
+
+##if [ ${LD_LIBRARY_PATH} == "" ]; then
+##	echo "export LD_LIBRARY_PATH=\$SYSTEMC/lib-\$TARGET_ARCH" >> SystemC.config
+##else
+##	echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$SYSTEMC/lib-\$TARGET_ARCH" >> SystemC.config
+##fi
+
+#echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}\$SYSTEMC/lib-\$TARGET_ARCH" >> SystemC.config
 
 echo "======================================================"
 echo "Build and Installation finished"
